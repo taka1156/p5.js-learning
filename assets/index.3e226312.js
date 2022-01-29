79b9d3cb0b0b409e4fab7d9027b2b348d7439c6f
taka1156 <taka1156@users.noreply.github.com>
2022-01-29 20:46:07 +0000

    deploy: 83a09aac84a6201c1c81cea1d79cc4c492210b99

--- assets/index.3e226312.js
+++ assets/index.3e226312.js
@@ -11,13 +11,12 @@ const i={heroText:"P5.js\u5B66\u7FD2\u30DA\u30FC\u30B8",leadText:"p5.js\u306E\u5
 <li>
 <details><summary>\u52D5\u304D\u3042\u308A(2D)</summary>
 <ul>
-<li><a href="./pages/2d/RandomBall.html">\u30DC\u30FC\u30EB\u3092\u52D5\u304B\u3059</a></li>
 <li><a href="./pages/2d/BlockBreak.html">\u30D6\u30ED\u30C3\u30AF\u5D29\u3057(PC)</a></li>
-<li><a href="./pages/2d/ColorChangedTime.html">\u6642\u9593\u3067\u8272\u5909\u3048</a></li>
-<li><a href="./pages/2d/ShakeChangedColor.html">\u632F\u3063\u3066\u8272\u5909\u3048(\u30B9\u30DE\u30DB)</a></li>
-<li><a href="./pages/2d/ShakeBalls.html">\u632F\u3063\u3066\u30DC\u30FC\u30EB\u3092\u51FA\u3059(\u30B9\u30DE\u30DB)</a></li>
-<li><a href="./pages/2d/CollisionBall.html">\u5186\u540C\u58EB\u3067\u5F53\u305F\u308A\u5224\u5B9A</a></li>
-<li><a href="./pages/2d/RotateArc.html">\u30ED\u30FC\u30C7\u30A3\u30F3\u30B0\u30A2\u30A4\u30B3\u30F3\u306E\u3088\u3046\u306A\u3082\u306E</a></li>
+<li><a href="./pages/2d/ColorChangedTime/index.html">\u6642\u9593\u3067\u8272\u5909\u3048</a></li>
+<li><a href="./pages/2d/ShakeChangedColor/index.html">\u632F\u3063\u3066\u8272\u5909\u3048(\u30B9\u30DE\u30DB)</a></li>
+<li><a href="./pages/2d/ShakeBalls/index.html">\u632F\u3063\u3066\u30DC\u30FC\u30EB\u3092\u51FA\u3059(\u30B9\u30DE\u30DB)</a></li>
+<li><a href="./pages/2d/CollisionBall.html">\u30DC\u30FC\u30EB\u3067\u5F53\u305F\u308A\u5224\u5B9A</a></li>
+<li><a href="./pages/2d/RotateArc/index.html">\u30ED\u30FC\u30C7\u30A3\u30F3\u30B0\u30A2\u30A4\u30B3\u30F3\u306E\u3088\u3046\u306A\u3082\u306E</a></li>
 </ul>
 </details>
 </li>
@@ -30,19 +29,11 @@ const i={heroText:"P5.js\u5B66\u7FD2\u30DA\u30FC\u30B8",leadText:"p5.js\u306E\u5
 </ul>
 </details>
 </li>
-<li>
-<details><summary>\u64CD\u4F5C\u7CFB</summary>
-<ul>
-<li><a href="./pages/event/Acceleration.html">\u52A0\u901F\u5EA6\u53D6\u5F97(\u30B9\u30DE\u30DB)</a></li>
-<li><a href="./pages/event/Rotation.html">\u50BE\u304D\u53D6\u5F97(\u30B9\u30DE\u30DB)</a></li>
 </ul>
-</details>
-</li>
-</ul>
-`,r=()=>{const{heroText:l,leadText:e,infoText:a,btnText:n,btnUrl:t}=i;document.getElementById("hero").innerHTML=`
+`,r=()=>{const{heroText:e,leadText:l,infoText:a,btnText:n,btnUrl:t}=i;document.getElementById("hero").innerHTML=`
     <div class="jumbotron">
-    <h1 class="display-5">${l}</h1>
-    <p class="lead">${e}</p>
+    <h1 class="display-5">${e}</h1>
+    <p class="lead">${l}</p>
     <hr class="my-2">
     <p>${a}</p>
     <a class="btn btn-primary btn-lg" href="${t}" role="button">
