63a03093b9eabb59b189bbdb9ef634ab9e1d2417
taka <tomo05061@gmail.com>
2022-02-23 07:20:52 +0900

    fix: 自作プラグインの引数追加

--- plugins/vite/markdownIt/index.ts
+++ plugins/vite/markdownIt/index.ts
@@ -8,14 +8,10 @@ const containerMdExtend = (md) => ({
     return params.trim().match(/^spoiler\s+(.*)$/);
   },
   render: function (tokens, idx) {
-    var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
-
+    const m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
     if (tokens[idx].nesting === 1) {
-      // opening tag
       return '<details><summary>' + md.utils.escapeHtml(m[1]) + '</summary>\n';
-
     } else {
-      // closing tag
       return '</details>\n';
     }
   }
